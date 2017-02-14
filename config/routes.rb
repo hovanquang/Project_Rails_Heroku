dbbf90909612d034b396c1bc0f9825482a94b931
hovanquang <vanquangtpm@gmail.com>
2017-02-14 15:30:35 +0700

    commit next

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
   resources :microposts
   resources :users
+
+ root 'users#index'
+  get 'home' , to: 'static_pages#home'
+  get '/help' , to: 'static_pages#help'
+  get '/about', to: 'static_pages#about'
+  get  '/contact', to: 'static_pages#contact'
+ 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
